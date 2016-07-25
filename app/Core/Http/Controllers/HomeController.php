457268a9abb8d137c1227b556c8ba59973f4349f
Nil Késede <nil@ksde.pw>
2016-07-25 11:26:16 -0300

    Add some comments

--- app/Core/Http/Controllers/HomeController.php
+++ app/Core/Http/Controllers/HomeController.php
@@ -4,6 +4,11 @@ namespace SisAdmin\Core\Http\Controllers;
 
 class HomeController extends Controller
 {
+    /**
+     * Show the application's homepage
+     *
+     * @return \Illuminate\View\View
+     */
     public function getIndex()
     {
         return view('welcome');
