a70ccbd87e8e06701a6705b8e0398a796bd265dc
Alison Monteiro <alisonmonteiro.10@gmail.com>
2016-10-23 22:56:09 -0300

    add users list

--- app/Users/Http/Controllers/UsersController.php
+++ app/Users/Http/Controllers/UsersController.php
@@ -2,25 +2,38 @@
 
 namespace SisAdmin\Users\Http\Controllers;
 
+use Auth;
 use SisAdmin\Core\Http\Controllers\Controller;
+use SisAdmin\Users\Entities\User;
 
 class UsersController extends Controller
 {
     /**
+     * @var \SisAdmin\Users\Entities\User
+     */
+    protected $user;
+
+    /**
      * Create a new controller instance.
      */
-    public function __construct()
+    public function __construct(User $user)
     {
         $this->middleware('auth');
+        $this->user = $user;
     }
 
     /**
-     * Show the dashboard panel.
+     * Show the users list.
      *
      * @return \Illuminate\View\View
      */
     public function getIndex()
     {
-        return view('users::index');
+        $userId = Auth::user()->id;
+        $users = $this->user->where('id', '!=', $userId)->orderBy('name')->paginate(50);
+
+        return view('users::index', [
+            'users' => $users,
+        ]);
     }
 }
--- app/Users/Resources/views/index.blade.php
+++ app/Users/Resources/views/index.blade.php
@@ -4,5 +4,35 @@
 @section('description', trans('users::info.description'))
 
 @section('content')
-    {{ trans('users::info.name') }}
+    <h3>{{ trans('users::info.name') }}</h3>
+
+    @if (isset($users) && count($users) > 0)
+        <table class="table table-striped table-hover module-table-index">
+            <thead>
+                <tr>
+                    <th width="30">#</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th width="100"></th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $count => $user)
+                <tr>
+                    <td>{{ $count + ($users->perPage() * ($users->currentPage()-1)) + 1 }}</td>
+                    <td><a href="{{ url('admin/users/' . $user->id . '/edit') }}">{{ $user->name }}</a></td>
+                    <td><a href="{{ url('admin/users/' . $user->id . '/edit') }}">{{ $user->email }}</a></td>
+                    <td nowrap>
+                        <a href="{{ url('admin/users/' . $user->id . '/edit') }}" class="module-action">Editar</a>&nbsp;
+                        <a href="#" class="module-action">Excluir</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="alert alert-warning">
+            <p>Nenhum item encontrado.</p>
+        </div>
+    @endif
 @endsection
